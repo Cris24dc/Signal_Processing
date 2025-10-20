5e81af68a9775aad267fa5ef181af4641d4f435a
Cris24dc <cristianandrei752@gmail.com>
2025-10-20 12:41:32 +0300

    Lab1 ex3

--- lab1/src/ex_1.py
+++ lab1/src/ex_1.py
@@ -2,12 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 1. a)
-t = np.linspace(0, 0.03, int(0.3/0.0005))
+t = np.linspace(0, 0.03, int(0.03/0.0005))
 
 # 1. b)
 x = np.cos(520*np.pi*t + np.pi/3)
 y = np.cos(280*np.pi*t - np.pi/3)
 z = np.cos(120*np.pi*t + np.pi/3)
+
+title = ['x(t)', 'y(t)', 'z(t)']
+funct = [x, y, z]
 
 fig, axs = plt.subplots(3)
 fig.suptitle('Semnalele x(t), y(t), z(t)')
@@ -16,27 +19,24 @@ for ax in axs.flat:
     ax.set_xlabel('Time')
     ax.set_ylabel('Amplitude')
 
-axs[0].plot(t, x)
-axs[0].set_title('x(t)')
-axs[0].set_ylim(-1.5, 1.5)
-
-axs[1].plot(t, y)
-axs[1].set_title('y(t)')
-axs[1].set_ylim(-1.5, 1.5)
-
-axs[2].plot(t, z)
-axs[2].set_title('z(t)')
-axs[2].set_ylim(-1.5, 1.5)
+for i in range(3):
+    axs[i].plot(t, funct[i])
+    axs[i].set_title(title[i])
+    axs[i].set_ylim(-1.5, 1.5)
 
 plt.tight_layout()
 plt.savefig('./img/Ex_1_b.pdf', format='pdf')
 
 # 1. c)
-fs = int(0.03/(1/200))
-t_e = np.linspace(0, 0.03, fs)
+fs = 200
+Te = 1/fs
+t_e = np.linspace(0, 0.03, int(0.03/Te))
 x_e = np.cos(520*np.pi*t_e + np.pi/3)
 y_e = np.cos(280*np.pi*t_e - np.pi/3)
 z_e = np.cos(120*np.pi*t_e + np.pi/3)
+
+title = ['x[n]', 'y[n]', 'z[n]']
+funct = [x_e, y_e, z_e]
 
 fig, axs = plt.subplots(3)
 fig.suptitle('Semnalele x[n], y[n], z[n]')
@@ -45,17 +45,10 @@ for ax in axs.flat:
     ax.set_xlabel('Time')
     ax.set_ylabel('Amplitude')
 
-axs[0].stem(t_e, x_e)
-axs[0].set_title('x[n]')
-axs[0].set_ylim(-1.5, 1.5)
-
-axs[1].stem(t_e, y_e)
-axs[1].set_title('y[n]')
-axs[1].set_ylim(-1.5, 1.5)
-
-axs[2].stem(t_e, z_e)
-axs[2].set_title('z[n]')
-axs[2].set_ylim(-1.5, 1.5)
+for i in range(3):
+    axs[i].stem(t_e, funct[i])
+    axs[i].set_title(title[i])
+    axs[i].set_ylim(-1.5, 1.5)
 
 plt.tight_layout()
 plt.savefig('./img/Ex_1_c.pdf', format='pdf')
--- lab1/src/ex_3.py
+++ lab1/src/ex_3.py
@@ -1,2 +1,9 @@
 # 3. a)
+# 2000 Hz -> 2000 de esantioane pe secunda
+# 1/2000 -> 0.0005 secunde
+
 # 3. b)
+# 1 ora -> 3600 secunde
+# 3600 * 2000 = 7200000 esantioane
+# 7200000 * 4 = 28800000 biti
+# 28800000 / 8 = 3600000 bytes
